23ccb65ab3353176e8a30fed399373b6132ae341
Alvin Pergens <alvin@pergens.fr>
2023-12-23 01:15:13 +0100

    Per View settings

--- timeout.js
+++ timeout.js
@@ -6,6 +6,7 @@ class ViewTimeout {
     this.llAttempts = 0;
 
     this.user;
+    this.views;
     this.pause;
     this.reset;
     this.homeView;
@@ -18,18 +19,25 @@ class ViewTimeout {
   }
 
   run(lovelace = this.main.querySelector("ha-panel-lovelace")) {
-    if (this.queryString("disable_timeout") || !lovelace) return;
+    if (this.queryString("disable_timeout") || !lovelace) {
+      return;
+    }
     this.getConfig(lovelace);
+
+    console.info("%c VIEWTIMEOUT %c v1.1.0 ", "color: orange; font-weight: bold; background: black", "color: white; font-weight: bold; background: dimgray");
   }
 
+  // initialise tous les éléments
   getConfig(lovelace) {
     this.llAttempts++;
 
     try {
       const llConfig = lovelace.lovelace.config;
       const config = llConfig.view_timeout || {};
+
       this.processUsers(lovelace, config);
       this.processReset(lovelace, config);
+      this.processViews(lovelace, config);
       this.processConfig(lovelace, config);
     } catch (e) {
       if (this.llAttempts < 200) {
@@ -42,12 +50,21 @@ class ViewTimeout {
     }
   }
 
+  // returns the destination view based on the current view
+  getTarget(name) {
+    const target = this.views[this.urlGetView()];
+    return (typeof target === 'string' || target === false) ? target : null;
+  }
+
+  // checks if the current user is authorized
   processUsers(lovelace, config) {
     try {
       this.user = {
         logged: this.ha.hass.user?.name.toLowerCase() || '',
         list: []
       };
+
+      // adds lowercase usernames to the array
       const users = config.users || [];
       users.forEach(user => this.user.list.push(user.toLowerCase()));
     } catch (e) {
@@ -56,6 +73,7 @@ class ViewTimeout {
     }
   }
 
+  // checks the different reset settings
   processReset(lovelace, config) {
     this.reset = {
       mouse_move: (typeof config.reset?.mouse_move === 'boolean') ? config.reset.mouse_move : false,
@@ -64,83 +82,104 @@ class ViewTimeout {
     };
   }
 
-  processConfig(lovelace, config) {
-    if (!config.timeout) return;
-    if (this.user.list.length && this.user.list.indexOf(this.user.logged) < 0) return;
+  // checks the destination of the views
+  processViews(lovelace, config) {
+    this.views = (typeof config.views === 'object') ? config.views : [];
+  }
 
-    this.homeView = config.default || "home";
+  // 
+  processConfig(lovelace, config) {
+    if (!config.timeout || (this.user.list.length && this.user.list.indexOf(this.user.logged) < 0)) {
+      return;
+    }
+
+    this.homeView = config.default;
     this.defaultPanelUrl = this.ha.hass.panelUrl;
 
     this.timeoutTime = config.duration || 30000;
     setTimeout(() => this.urlCheckerStart(), 50);
   }
 
-  // Convert to array.
+  // convert to array.
   array(x) {
     return Array.isArray(x) ? x : [x];
   }  
 
+  // checks if the keys are present in the query
   queryString(keywords) {
     return this.array(keywords).some((x) => window.location.search.includes(x));
   }
 
+  // resets the countdown when an event is raised
   resetEvent() {
-    clearTimeout(this.ViewTimeout.viewTimeout);
-    this.ViewTimeout.viewTimeout = setTimeout(() => this.ViewTimeout.timeoutReturn(), this.ViewTimeout.timeoutTime);
+    clearTimeout(this.viewTimeout);
+    this.viewTimeout = setTimeout(() => this.timeoutReturn(), this.timeoutTime);
   }
 
+  // activates the countdown
   setViewTimeout() {
     if (this.reset.mouse_move) {
-      window.addEventListener("mousemove", this.resetEvent);
+      window.addEventListener("mousemove", () => this.resetEvent());
     }
 
     if (this.reset.mouse_click) {
-      window.addEventListener("click", this.resetEvent);
+      window.addEventListener("click", () => this.resetEvent());
     }
 
     this.viewTimeout = setTimeout(() => this.timeoutReturn(), this.timeoutTime);
   }
 
+  // deactivates the countdown
   cancelEverything() {
     window.removeEventListener("mousemove", this.resetEvent);
     window.removeEventListener("click", this.resetEvent);
     clearTimeout(this.viewTimeout);
-    //null the timeout
+    // null the timeout
     this.viewTimeout = false;
   }
 
+  // redirects once the countdown is complete
   timeoutReturn() {
     this.cancelEverything();
 
-    //Remove focus from the former active tab to clear the style
+    // remove focus from the former active tab to clear the style
     try {
-      var activeTab = this.main.querySelector('ha-drawer > partial-panel-resolver > ha-panel-lovelace').shadowRoot.querySelector('hui-root').shadowRoot.activeElement;
-      if (activeTab != null) activeTab.blur();
+      const activeTab = this.main.querySelector("ha-drawer > partial-panel-resolver > ha-panel-lovelace").shadowRoot.querySelector("hui-root").shadowRoot.activeElement;
+      if (activeTab != null) {
+        activeTab.blur();
+      }
     } catch (e) {
-      console.log("Failed to blur active tab:" + e);
+      console.log("Failed to blur active tab: " + e);
     }
 
-    //switch tabs
-    window.history.pushState("", "", "/" + this.defaultPanelUrl + "/" + this.homeView);
+    // switch tabs
+    const target = this.getTarget(this.urlGetView()) || "home";
+    window.history.pushState("", "", "/" + this.defaultPanelUrl + "/" + target);
     window.cardTools.fireEvent("location-changed", {}, document.querySelector("home-assistant"));
   }
 
   urlCheckerStart() {
-   this.urlInterval = setInterval(() => this.urlChecker(), 1000);
+    this.urlInterval = setInterval(() => this.urlChecker(), 1000);
   }
 
+  // returns the id of the current view
   urlGetView() {
     return window.location.pathname.split("/").pop();
   }
 
+  // indicates if you are on the Lovelace dashboard
   isDefaultPanel() {
     return window.location.pathname === "/" + this.defaultPanelUrl || window.location.pathname.startsWith("/" + this.defaultPanelUrl + "/");
   }
 
+  // activates or deactivates the countdown depending on the different settings
   urlChecker() {
-    const in_lovelace = !this.reset.in_lovelace || this.isDefaultPanel();
+    // if the current view is not the default view, or if this view has destination "false" (false disables redirection to the default)
+    const isTargetedView = this.homeView === this.urlGetView() || this.getTarget(this.urlGetView()) === false;
+    // if the variable "in_lovelace" is "false" (therefore the redirection works in any Home Assistant) or that we are in the lovelace dashboard
+    const inLovelace = !this.reset.in_lovelace || this.isDefaultPanel();
 
-    if (in_lovelace && this.homeView != this.urlGetView()) {
+    if (!isTargetedView && inLovelace) {
       if (!this.viewTimeout) {
         this.setViewTimeout();
       }
@@ -152,7 +191,7 @@ class ViewTimeout {
   }
 }
 
-// Initial Run
+// initial run
 Promise.resolve(customElements.whenDefined("hui-view")).then(() => {
   window.ViewTimeout = new ViewTimeout();
 });
